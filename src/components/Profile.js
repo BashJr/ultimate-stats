37d0499f42c95c393b8a2b2106ca56519e8f54d7
Zach Watts <zacharymwatts@gmail.com>
2022-05-12 11:58:56 -0700

    Creating project structure

---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Profile extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div class="profile-container">
+                <h2 class="name">Zach Watts</h2>
+            </div>
+        )
+    }
+}
